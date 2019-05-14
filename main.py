0c4b27963e1728ac6be5154b87c467147c9ce612
Volodymyr Chernetskyi <v.v.chernetskyi@gmail.com>
2019-05-15 02:50:30 +0300

    Add linear regression and dataset
    Include dataset of 40 MRI brain scans. Implement linear regression in main.py.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
 from PIL import Image
-from sklearn.decomposition import NMF
 from functools import reduce
+from sklearn.decomposition import NMF
+from sklearn.linear_model import LinearRegression
 
 
 def get_image_line(img_name):
@@ -14,8 +15,18 @@ def add_line(arr, line):
 
 
 if __name__ == '__main__':
-    matrix = reduce(add_line, [get_image_line(f'testdata/img{i + 1}.png') for i in range(9)]).transpose()
+    matrix = reduce(add_line, [get_image_line(f'dataset/{i + 1}.png') for i in range(40)]).transpose()
+    # get data
     model = NMF(2)
     w = model.fit_transform(matrix)
     h = model.components_
     y = np.matmul(w, h)
+    # non negative matrix factorization
+    age_array = np.empty(40, np.float_)
+    with open('dataset/dataset.csv') as file:
+        for i, line in enumerate(file):
+            age_array[i] = np.float_(line.split()[1])
+    # get ages
+    reg = LinearRegression().fit(h.transpose()[:30], age_array[:30])
+    print(reg.score(h.transpose()[30:], age_array[30:]))
+    # get score
